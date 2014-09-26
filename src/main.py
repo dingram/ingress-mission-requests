fb7a2e4ad585b25269c5ed79c05a8ee22cf56127
Dave Ingram <dave@dmi.me.uk>
2014-09-25 20:52:22 -0700

    Added basic landing page

---
+++ src/main.py
@@ -0,0 +1,34 @@
+import logging
+import os
+import sys
+import webapp2
+
+from google.appengine.ext import ndb
+
+import fix_path
+
+import app.views.simple
+
+
+DEBUG_MODE = (os.environ.get('SERVER_SOFTWARE', '').startswith('Development')
+    or 'test' in os.environ.get('HTTP_HOST', os.environ.get('SERVER_NAME')))
+
+
+application = ndb.toplevel(webapp2.WSGIApplication([
+  webapp2.Route('/', app.views.simple.SimplePage, 'landing', defaults={'_template': 'index.html'}),
+
+  webapp2.Route('/login',  app.views.simple.Login,  'login'),
+  webapp2.Route('/logout', app.views.simple.Logout, 'logout'),
+
+  (r'.*', app.views.simple.NotFound),
+], debug=DEBUG_MODE))
+
+
+def main():
+  logging.getLogger().setLevel(logging.INFO)
+  application.run()
+
+if __name__ == "__main__":
+  main()
+
+# vim: et sw=2 ts=2 sts=2 cc=80
