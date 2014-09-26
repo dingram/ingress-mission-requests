03251f2239793902409d9114b4dcf95a2a242ab7
Dave Ingram <dave@dmi.me.uk>
2014-09-25 23:03:11 -0700

    Implement user signup

--- src/app/views/user.py
+++ src/app/views/user.py
@@ -1,4 +1,5 @@
 import logging
+import re
 
 from google.appengine.api import users
 
@@ -19,6 +20,40 @@ class Signup(RequestHandler):
   def get(self, *args, **kwargs):
     self.user = models.User.get_current(allow_creation=True)
     self.render_page('signup.html')
+
+  def post(self, *args, **kwargs):
+    nickname = self.request.POST.get('nickname')
+    faction = self.request.POST.get('faction')
+    error = None
+
+    self.user = models.User.get_current(allow_creation=True)
+    try:
+      self.check_xsrf_token(required=False)
+    except:
+      error = 'Invalid XSRF token'
+
+    self.user.nickname = nickname
+    self.user.faction = faction
+
+    if not error and not nickname:
+      error = 'No nickname'
+    if not error and not re.match(r'[0-9a-zA-Z]{3,15}', nickname):
+      error = 'Invalid nickname'
+    if not error and not faction:
+      error = 'No faction'
+    if not error and faction not in ('ENLIGHTENED', 'RESISTANCE'):
+      error = 'Invalid faction'
+    if not error:
+      u = models.User.fetch_by_nickname(nickname)
+      if u and u.guid != self.user.guid:
+        error = 'Nickname is already registered'
+
+    if not error:
+      self.user.put()
+      self.redirect('/', abort=303)
+      return
+    else:
+      self.render_page('signup.html', {'error': error})
 
 
 # vim: et sw=2 ts=2 sts=2 cc=80
